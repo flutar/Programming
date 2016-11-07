bdebc4805b5f4697bb052c14682ae3d52f0af429
Michael <yu.zhanchao@gmail.com>
2016-11-07 19:22:52 +0800

    added author description part

--- wordpress/wp-content/themes/onepress/template-parts/content-single.php
+++ wordpress/wp-content/themes/onepress/template-parts/content-single.php
@@ -17,12 +17,38 @@
 		<div class="entry-meta">
 			<?php onepress_posted_on(); ?>
 		</div><!-- .entry-meta -->
-		
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
+
+		<?php /* Check the authors' other posts*/ ?>
+
+		<?php $otherAuthorPosts = new WP_Query(array(
+			'author' => get_the_author_meta('ID'), 
+			'posts_per_page' => 3, 
+			'post__not_in' => array(get_the_ID())
+		)); ?>
+
+		<div class="about-author clearfix">
+		 	<div class="about-author-image">
+		 		<?php echo get_avatar(get_the_author_meta('ID'), 512) ?>
+		 		<?php echo get_the_author_meta('nickname') ?>
+		 	</div>
+		 	<div class="about-author-text">
+		 		<?php echo wpautop(get_the_author_meta('description')) ?>
+					<ul>
+						<?php while ($otherAuthorPosts->have_posts()) {
+							$otherAuthorPosts->the_post(); ?>
+							<li><a href="<?php the_permalink() ?>"><?php the_title() ?></a></li>
+						<?php } ?>
+					</ul>
+		 	</div>
+		</div>
+
+
 
 	<footer class="entry-footer">
 		<?php onepress_entry_footer(); ?>
