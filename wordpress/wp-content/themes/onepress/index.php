e6e8e0e6e8d1cc94024aa2c4656f0785fbc9e280
flutar <yu.zhanchao@gmail.com>
2016-11-14 22:08:07 +0800

    I did not change anything.

--- wordpress/wp-content/themes/onepress/index.php
+++ wordpress/wp-content/themes/onepress/index.php
@@ -6,7 +6,7 @@
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
- * 
+ * I did not add anything new.
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package OnePress & Michael 
