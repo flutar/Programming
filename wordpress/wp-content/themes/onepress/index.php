296a2b6657b8f4851caccc6486beab9d518b7d03
Michael Yu <Michael@192.168.0.105>
2016-11-14 22:00:34 +0800

    I just added.

--- wordpress/wp-content/themes/onepress/index.php
+++ wordpress/wp-content/themes/onepress/index.php
@@ -6,10 +6,10 @@
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
- *
+ * 
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package OnePress
+ * @package OnePress & Michael 
  */
 
 get_header();
